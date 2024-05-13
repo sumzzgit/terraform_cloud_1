4583afb8d5d0ce7ac72d060f0e350edd49e0ba9c
sumzz <sumitkotabagi@gmial.com>
2024-05-13 10:47:57 +0530

    inita; commit

---
+++ data.tf
@@ -0,0 +1,16 @@
+# import vpc details 
+data "aws_vpc" "sumzz_vpc" {
+  id = var.vpc_id
+}
+
+# import public subnet details 
+data "aws_subnet" "pub-sub-01" {
+  id = var.subnet_id
+}
+
+#import security group details 
+data "aws_security_groups" "test" {
+  tags = {
+    Name = "pub-sg-1"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_id" {
+  default = "vpc-0a7717572bd1cc943"
+}
+
+variable "subnet_id" {
+  default = "subnet-0eb6beb325d7fd8ba"
+}
